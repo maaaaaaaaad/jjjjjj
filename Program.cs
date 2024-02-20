13d69e8e0d4974ddad7875ec9bd5134c622999fa
maaaaaaaaad <integral5137@gmail.com>
2024-02-20 18:14:42 +0900

    e

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MyApp
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MyApp
 {
     class Program
     {
@@ -25,6 +27,23 @@
             if(ReferenceEquals(null, other)) return false;
             if(ReferenceEquals(this, other)) return true;
             return string.Equals(FirstName, other.FirstName) && string.Equals(LastName, other.LastName);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(ReferenceEquals(null, obj)) return false;
+            if(ReferenceEquals(this, obj)) return true;
+            if(obj.GetType() != this.GetType()) return false;
+            return Equals((FullName) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((FirstName != null ? FirstName.GetHashCode():0) * 397)
+                    ^ (LastName != null ? LastName.GetHashCode(): 0);
+            }
         }
     }
 } 
