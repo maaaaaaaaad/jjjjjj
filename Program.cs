8ac4b7731e476d148d829a6973814aca49aadb75
maaaaaaaaad <integral5137@gmail.com>
2024-02-20 18:11:22 +0900

    e

--- Program.cs
+++ Program.cs
@@ -1,17 +1,15 @@
-﻿using System;
-
-namespace MyApp
+﻿namespace MyApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var fullname = new FullName("Super", "Fortess");
+            var fullName = new FullName("Super", "Fortess");
             Console.WriteLine($"Full Name: {fullName.FirstName} {fullName.LastName}");
         }
     }
 
-    class FullName
+    class FullName : IEquatable<FullName>
     {   
         public FullName(string firstName, string lastName)
         {
@@ -21,5 +19,12 @@ namespace MyApp
         
         public string FirstName {get;}
         public string LastName {get;}
-    } 
+
+        public bool Equals(FullName other)
+        {
+            if(ReferenceEquals(null, other)) return false;
+            if(ReferenceEquals(this, other)) return true;
+            return string.Equals(FirstName, other.FirstName) && string.Equals(LastName, other.LastName);
+        }
+    }
 } 
