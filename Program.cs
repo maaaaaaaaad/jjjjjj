fa0476add5f204446f43a47b533a8eaf0b93d9b8
maaaaaaaaad <integral5137@gmail.com>
2024-02-20 18:54:50 +0900

    e

--- Program.cs
+++ Program.cs
@@ -12,38 +12,39 @@ namespace MyApp
     }
 
     class FullName : IEquatable<FullName>
-    {   
+    {
         public FullName(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
         }
-        
-        public string FirstName {get;}
-        public string LastName {get;}
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
 
         public bool Equals(FullName other)
         {
-            if(ReferenceEquals(null, other)) return false;
-            if(ReferenceEquals(this, other)) return true;
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
             return string.Equals(FirstName, other.FirstName) && string.Equals(LastName, other.LastName);
         }
 
         public override bool Equals(object obj)
         {
-            if(ReferenceEquals(null, obj)) return false;
-            if(ReferenceEquals(this, obj)) return true;
-            if(obj.GetType() != this.GetType()) return false;
-            return Equals((FullName) obj);
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((FullName)obj);
         }
-
+        
         public override int GetHashCode()
         {
             unchecked
             {
-                return ((FirstName != null ? FirstName.GetHashCode():0) * 397)
-                    ^ (LastName != null ? LastName.GetHashCode(): 0);
+                return ((FirstName != null ? FirstName.GetHashCode() : 0) * 397)
+                       ^ (LastName != null ? LastName.GetHashCode() : 0);
             }
         }
     }
-} 
+}
