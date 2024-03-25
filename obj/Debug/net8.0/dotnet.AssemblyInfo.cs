5c701a8739921b7cd26a4e528b4e34445b39bad0
maaaaaaaaad <integral5137@gmail.com>
2024-03-25 16:04:30 +0900

    e

--- obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3365b4f041af4c7c4050bae313708727c3fd8da0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acda408764beccce3a3364bd1082355a2c4934f")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
